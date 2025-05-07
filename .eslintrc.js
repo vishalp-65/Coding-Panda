a31c90146d6e59ffaf1b8eebbe6e4d8f6c33dc4e
Vishal Panchal <vishalpanchal1828vp@gmail.com>
2025-05-08 00:57:44 +0530

    Initilized packages

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    '@typescript-eslint/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/explicit-function-return-type': 'warn',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/prefer-const': 'error',
+    'no-console': 'warn',
+    'prefer-const': 'error',
+  },
+  env: {
+    node: true,
+    es2022: true,
+  },
+  ignorePatterns: [
+    'dist/',
+    'node_modules/',
+    '*.js',
+  ],
+};
